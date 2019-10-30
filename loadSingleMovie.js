3d0965b80083006ba17495f1b250668e0b60deda
George Niece <george.niece@digitaltransformationstrategies.net>
2019-10-30 04:48:07 +0000

    Fix error with loadSingleMovie.js

--- loadSingleMovie.js
+++ loadSingleMovie.js
@@ -1,4 +1,3 @@
-// Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
@@ -9,10 +8,11 @@ var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 var params = {
   TableName: 'MOVIES5',
   Item: {
-
+    'year' : {N: '1983'},
+    'title' : {S: 'They Call Me Bruce'}
   }
 };
-
+console.log(params)
 // Call DynamoDB to add the item to the table
 ddb.putItem(params, function(err, data) {
   if (err) {
